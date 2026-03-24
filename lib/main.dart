f08ef4285bb3b4667878838716bb9e8f2fc13a50
a.23308051280644 <a.23308051280644@cbtis128.edu.mx>
2026-03-24 14:33:44 +0000

    Otra vez

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,104 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const DominosApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class DominosApp extends StatelessWidget {
+  const DominosApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: const DominosGrid(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class DominosGrid extends StatelessWidget {
+  const DominosGrid({super.key});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  // Lista de datos simulados para 14 productos
+  final List<Map<String, String>> productos = const [
+    {"nombre": "Pizza Pepperoni", "desc": "Clásica y deliciosa", "img": "https://raw.githubusercontent.com/Mirelesalexis/imagen-dominios2/refs/heads/main/Gemini_Generated_Image_nx38aunx38aunx38.png"},
+    {"nombre": "Pizza Hawiana", "desc": "Piña y jamón", "img": "https://raw.githubusercontent.com/Mirelesalexis/imagen-dominios2/refs/heads/main/Gemini_Generated_Image_otmxeaotmxeaotmx.png"},
+    {"nombre": "Pizzeta de Carne", "desc": "Carne a elegir", "img": "https://raw.githubusercontent.com/Mirelesalexis/imagen-dominios2/refs/heads/main/Gemini_Generated_Image_ui042oui042oui04.png"},
+    {"nombre": "Pizza de Queso", "desc": "Queso fresco", "img": "https://raw.githubusercontent.com/Mirelesalexis/imagen-dominios2/refs/heads/main/Gemini_Generated_Image_z2g72jz2g72jz2g7.png"},
+    {"nombre": "Papas Fritas", "desc": "Complementos", "img": "https://raw.githubusercontent.com/Mirelesalexis/imagen-dominios2/refs/heads/main/Gemini_Generated_Image_3laqno3laqno3laq.png"},
+    {"nombre": "Pepsi 500ml", "desc": "Bebida individual", "img": "https://raw.githubusercontent.com/Mirelesalexis/imagen-dominos/refs/heads/main/Gemini_Generated_Image_43pfrj43pfrj43pf.png"},
+    {"nombre": "Fanta 500ml", "desc": "Bebida individual", "img": "https://raw.githubusercontent.com/Mirelesalexis/imagen-dominos/refs/heads/main/Gemini_Generated_Image_1mh85n1mh85n1mh8.png"},
+    {"nombre": "Coca-Cola 500ml", "desc": "Bebida individual", "img": "https://raw.githubusercontent.com/Mirelesalexis/imagen-dominos/refs/heads/main/Gemini_Generated_Image_g0njrrg0njrrg0nj.png"},
+    {"nombre": "Sprite", "desc": "Bebida individual", "img": "https://raw.githubusercontent.com/Mirelesalexis/imagen-dominos/refs/heads/main/Gemini_Generated_Image_pv23zspv23zspv23.png"},
+    {"nombre": "Aros de cebolla", "desc": "Complementos", "img": "https://raw.githubusercontent.com/Mirelesalexis/imagen-dominos/refs/heads/main/aros.jpg"},
+    {"nombre": "Dedos de Queso", "desc": "Complementos", "img": "https://raw.githubusercontent.com/Mirelesalexis/imagen-dominos/refs/heads/main/ded.jpg"},
+    {"nombre": "Boneless BBQ", "desc": "Complementos", "img": "https://raw.githubusercontent.com/Mirelesalexis/imagen-dominos/refs/heads/main/bbqqq.png"},
+    {"nombre": "Boneless Bufalo", "desc": "Complementos", "img": "https://raw.githubusercontent.com/Mirelesalexis/imagen-dominos/refs/heads/main/fs.png"},
+    {"nombre": "Pepsi 3L", "desc": "Bebida familiar", "img": "https://raw.githubusercontent.com/Mirelesalexis/imagen-dominos/refs/heads/main/Peppp.png"},
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Dominos Grid Menu - Alexis'),
+        backgroundColor: const Color(0xFF006491), // Azul Dominos
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: GridView.builder(
+        padding: const EdgeInsets.all(10),
+        // Definimos las 2 columnas
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 0.75, // Ajusta la altura de las tarjetas
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
         ),
+        itemCount: productos.length,
+        itemBuilder: (context, index) {
+          return Card(
+            elevation: 4,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Imagen Superior
+                Expanded(
+                  child: ClipRRect(
+                    borderRadius: const BorderRadius.vertical(top: Radius.circular(15)),
+                    child: Image.network(
+                      productos[index]['img']!,
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                    ),
+                  ),
+                ),
+                // Información Inferior (Título, Subtítulo, Estrellas)
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        productos[index]['nombre']!,
+                        style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                      ),
+                      Text(
+                        productos[index]['desc']!,
+                        style: const TextStyle(color: Colors.grey, fontSize: 12),
+                      ),
+                      const SizedBox(height: 5),
+                      Row(
+                        children: List.generate(5, (starIndex) {
+                          return const Icon(Icons.star, color: Colors.amber, size: 16);
+                        }),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
